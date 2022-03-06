58cdb48a0c05302be95517795a7f6be510675e08
Tyson PHP Dev <36503515+TysonMot@users.noreply.github.com>
2022-03-06 20:55:52 +0200

    Modifications

--- src/bmeTest.java
+++ src/bmeTest.java
@@ -11,14 +11,14 @@ class bmeTest {
     @Test
     void primes() {
         prime items = new prime();
-        ArrayList<Integer> results = new ArrayList<Integer>(Arrays.asList(1,3,5,7,9,11,13));
-        Assertions.assertEquals(items.values(12), results);
+        ArrayList<Integer> results = new ArrayList<Integer>(Arrays.asList(1,3,5,7,9,11));
+        Assertions.assertEquals(items.values(10), results);
     }
 
     @Test
     void fibonacci() {
         Fibonaccis Fibo = new Fibonaccis();
-        ArrayList<Integer> results = new ArrayList<Integer>(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89));
-        Assertions.assertEquals( Fibo.Fibonacci(12), results);
+        ArrayList<Integer> results = new ArrayList<Integer>(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34));
+        Assertions.assertEquals( Fibo.Fibonacci(10), results);
     }
 }
